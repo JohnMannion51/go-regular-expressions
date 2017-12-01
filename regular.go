577311107f89377c01b77e975397ef0ef128a6c8
jono mannion <johnmannion51@gmail.com>
2017-12-01 18:53:09 +0000

    still working on reflections

--- regular.go
+++ regular.go
@@ -5,6 +5,7 @@ import (
 	"math/rand"
 	"time"
 	"regexp"
+	"strings"
 )
 
 // Adapted from: https://golang.org/pkg/math/rand/
@@ -26,22 +27,53 @@ func ElizaResponse(inputStr string) string {
 		"I’m not sure what you’re trying to say. Could you explain it to me?",
 		"How does that make you feel?",
 		"Why do you say that?",
+		//"How do you know that you are not sure that I understand the effect my questions are having on you.",
 	}
 	response := "User input :" + input + " \nOutput :" + answers[rand.Intn(len(answers))]
 	return response
 }
 
+func Reflect(inputStr string) string {
+	// Split the input on word boundaries.
+	input := inputStr
+	boundaries := regexp.MustCompile(`\b`)
+	tokens := boundaries.Split(input, -1)
+	
+	// List the reflections.
+	reflections := [][]string{
+		{`I`, `you`},
+		{`me`, `you`},
+		{`you`, `me`},
+		{`my`, `your`},
+		{`your`, `my`},
+		
+	}
+	
+	// Loop through each token, reflecting it if there's a match.
+	for i, token := range tokens {
+		for _, reflection := range reflections {
+			if matched, _ := regexp.MatchString(reflection[0], token); matched {
+				tokens[i] = reflection[1]
+				break
+			}
+		}
+	}
+	
+	// Put the tokens back together.
+	return strings.Join(tokens, ``)
+}
+
 func main() {
 	userInput := []string{
-				"People say I look like both my mother and father.",
+				/*"People say I look like both my mother and father.",
 				"Father was a teacher.",
 				"I was my father’s favourite.",
 				"I'm looking forward to the weekend.",
 				"My grandfather was French!",
 				"I am happy.",
-				"I am not happy with your responses.",
+				"I am not happy with your responses.",*/
 				"I am not sure that you understand the effect that your questions are having",
-				"I am supposed to just take what you're saying at face value?",
+				//"I am supposed to just take what you're saying at face value?",
 
 			}
 		rand.Seed(time.Now().UTC().UnixNano())
