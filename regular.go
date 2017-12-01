386878f7df8599cdbc44bce7cfdd3ed8fe8b2692
jono mannion <johnmannion51@gmail.com>
2017-12-01 19:19:06 +0000

    still working on reflections

--- regular.go
+++ regular.go
@@ -27,7 +27,9 @@ func ElizaResponse(inputStr string) string {
 		"I’m not sure what you’re trying to say. Could you explain it to me?",
 		"How does that make you feel?",
 		"Why do you say that?",
-		//"How do you know that you are not sure that I understand the effect my questions are having on you.",
+		"How often do you see him",
+		"How do you know that you are not sure that I understand the effect my questions are having on you.",
+		"",
 	}
 	response := "User input :" + input + " \nOutput :" + answers[rand.Intn(len(answers))]
 	return response
@@ -65,15 +67,18 @@ func Reflect(inputStr string) string {
 
 func main() {
 	userInput := []string{
-				/*"People say I look like both my mother and father.",
+				"People say I look like both my mother and father.",
 				"Father was a teacher.",
 				"I was my father’s favourite.",
 				"I'm looking forward to the weekend.",
 				"My grandfather was French!",
+				"I enjoy visiting my grandfather",
 				"I am happy.",
-				"I am not happy with your responses.",*/
+				"I am not happy with your responses.",
 				"I am not sure that you understand the effect that your questions are having",
-				//"I am supposed to just take what you're saying at face value?",
+				"I am supposed to just take what you're saying at face value?",
+				
+				
 
 			}
 		rand.Seed(time.Now().UTC().UnixNano())
@@ -82,41 +87,5 @@ func main() {
 		
 		fmt.Print(word)
 
-	/*rand.Seed(time.Now().UTC().UnixNano())
 
-	fmt.Println("People say I look like both my mother and father.")
-	fmt.Println(ElizaResponse("People say I look like both my mother and father."))
-	fmt.Println()
-
-	fmt.Println("Father was a teacher.")
-	fmt.Println(ElizaResponse("Father was a teacher."))
-	fmt.Println()
-
-	fmt.Println("I was my father’s favourite.")
-	fmt.Println(ElizaResponse("I was my father’s favourite."))
-	fmt.Println()
-
-	fmt.Println("I’m looking forward to the weekend.")
-	fmt.Println(ElizaResponse("I’m looking forward to the weekend."))
-	fmt.Println()
-
-	fmt.Println("My grandfather was French!")
-	fmt.Println(ElizaResponse("My grandfather was French!"))
-	fmt.Println()
-
-	fmt.Println("I am happy.")
-	fmt.Println(ElizaResponse("I am happy."))
-	fmt.Println()
-
-	fmt.Println("I am not happy with your responses.")
-	fmt.Println(ElizaResponse("I am not happy with your responses."))
-	fmt.Println()
-
-	fmt.Println("I am not sure that you understand the effect that your questions are having on me.")
-	fmt.Println(ElizaResponse("I am not sure that you understand the effect that your questions are having on me."))
-	fmt.Println()
-
-	fmt.Println("I am supposed to just take what you’re saying at face value?")
-	fmt.Println(ElizaResponse("I am supposed to just take what you’re saying at face value?"))
-	fmt.Println()*/
 }
